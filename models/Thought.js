2db87245c4d45069ee6953fc1c5042140e48af66
Koby <kobysac@gmail.com>
2023-12-12 00:10:09 -0800

    filled the models

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,70 @@
 const { Schema, model } = require('mongoose');
+const { DateTime } = require('luxon');
+
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: new Schema.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: DateTime,
+    default: DateTime.now(),
+    get: function (value) {
+      // ISO 8601 format (UTC)
+      return value.toFormat('yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'');
+    }
+  }
+});
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: DateTime,
+      default: DateTime.now(),
+      get: function (value) {
+        // ISO 8601 format (UTC)
+        return value.toFormat('yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\'');
+      }
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `reactionCount` that gets the amount of reactions per tjpught
+thoughtSchema
+  .virtual('reactionCount')
+  // Getter
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,54 @@
 const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(value);
+        },
+        message: props => `${props.value} is not a valid email address!`
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.thoughts.length;
+  })
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
